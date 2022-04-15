7ba35f35b7466a946612aa6ae9f1346003034ba0
nartesfasrum <nart@tirefire.pw>
2022-04-15 10:33:23 -0500

    mvp with pinning

--- hurq.sh
+++ hurq.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-CHANNEL=https://www.youtube.com/user/hickok45/videos
+USER_ID=
+CHANNEL_ID=
+CHANNEL_TYPE=1
 CACHE=videos.txt
 
-yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL -o '%(channel)s/%(title)s.%(ext)s'
+ipfs pin ls --type recursive | cut -d' ' -f1 | xargs -n1 ipfs pin rm
+ipfs repo gc
+
+if [ "$CHANNEL_TYPE" = "0" ]
+then
+	CHANNEL_URL=https://www.youtube.com/channel/$CHANNEL_ID/videos
+	CHANNEL_DIR=$CHANNEL_ID
+	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -o '%(channel_id)s/%(title)s.%(ext)s'
+fi
+if [ "$CHANNEL_TYPE" = "1" ]
+then
+	CHANNEL_URL=https://www.youtube.com/user/$USER_ID/videos
+	CHANNEL_DIR=$USER_ID
+	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -o '%(channel)s/%(title)s.%(ext)s'
+fi
+
+ipfs add -r $CHANNEL_DIR
+ipfs pin ls --type recursive > ~/hurq-hash.txt
