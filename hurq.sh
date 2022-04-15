11bc16e2d988385aaef2da2f70d38fbb374dc242
nartesfasrum <nart@tirefire.pw>
2022-04-15 12:02:04 -0500

    improve video download path

--- hurq.sh
+++ hurq.sh
@@ -3,23 +3,27 @@
 USER_ID=
 CHANNEL_ID=
 CHANNEL_TYPE=1
+
 CACHE=videos.txt
 HASH_PATH=~/hurq-hash.txt
+VIDEO_PATH=~/hurq/videos
 
 ipfs pin ls --type recursive | cut -d' ' -f1 | xargs -n1 ipfs pin rm
 ipfs repo gc
 
+mkdir -p $VIDEO_PATH
+
 if [ "$CHANNEL_TYPE" = "0" ]
 then
 	CHANNEL_URL=https://www.youtube.com/channel/$CHANNEL_ID/videos
-	CHANNEL_DIR=$CHANNEL_ID
-	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -o '%(channel_id)s/%(title)s.%(ext)s'
+	CHANNEL_DIR="${VIDEO_PATH}/${CHANNEL_ID}"
+	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -P $VIDEO_PATH -o '%(channel_id)s/%(title)s.%(ext)s'
 fi
 if [ "$CHANNEL_TYPE" = "1" ]
 then
 	CHANNEL_URL=https://www.youtube.com/user/$USER_ID/videos
-	CHANNEL_DIR=$USER_ID
-	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -o '%(channel)s/%(title)s.%(ext)s'
+	CHANNEL_DIR="${VIDEO_PATH}/${USER_ID}"
+	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -P $VIDEO_PATH -o '%(channel)s/%(title)s.%(ext)s'
 fi
 
 ipfs add -r $CHANNEL_DIR
