a4aa73e0dd6009097a4a4d8dbd6d95aeabfc7194
nartesfasrum <nart@tirefire.pw>
2022-04-27 19:03:36 -0500

    simplify path variables

--- example-config.sh
+++ example-config.sh
@@ -3,6 +3,4 @@ CHANNEL_TYPE=1				# 0 for CHANNEL_IDs from /channel/, 1 for CHANNEL_IDs from /c/
 CHANNEL_ID=UCDFD8RdIL2FxNfkKkus5RSQ	# channel ID - typically one of 3 types: a /channel/, a /c/, or a /user/
 
 # paths
-CACHE=~/hurq/downloaded-videos.txt	# path for the list of already downloaded videos
-HASH_PATH=~/hurq/hurq-hash.txt		# path for the hurq hash output
-VIDEO_PATH=~/hurq/videos		# path to download videos into
+HURQ_PATH=~/hurq/videos			# path for hurq to work in
--- hurq.sh
+++ hurq.sh
@@ -2,6 +2,10 @@
 
 source config.sh
 
+CACHE_PATH=$HURQ_PATH/downloaded-videos.txt
+HASH_PATH=$HURQ_PATH/hurq-hash.txt
+VIDEO_PATH=$HURQ_PATH/videos
+
 if [[ $(ipfs pin ls --type recursive) ]]
 then
 	ipfs pin ls --type recursive | cut -d' ' -f1 | xargs -n1 ipfs pin rm
@@ -19,19 +23,19 @@ if [ "$CHANNEL_TYPE" = "0" ]
 then
 	CHANNEL_URL=https://www.youtube.com/channel/$CHANNEL_ID/videos
 	CHANNEL_DIR="${VIDEO_PATH}/${CHANNEL_ID}"
-	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -P $VIDEO_PATH -o '%(channel_id)s/%(title)s.%(ext)s'
+	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE_PATH $CHANNEL_URL -P $VIDEO_PATH -o '%(channel_id)s/%(title)s.%(ext)s'
 fi
 if [ "$CHANNEL_TYPE" = "1" ]
 then
 	CHANNEL_URL=https://www.youtube.com/c/$CHANNEL_ID/videos
 	CHANNEL_DIR="${VIDEO_PATH}/${CHANNEL_ID}"
-	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -P $VIDEO_PATH -o '%(channel)s/%(title)s.%(ext)s'
+	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE_PATH $CHANNEL_URL -P $VIDEO_PATH -o '%(channel)s/%(title)s.%(ext)s'
 fi
 if [ "$CHANNEL_TYPE" = "2" ]
 then
 	CHANNEL_URL=https://www.youtube.com/user/$CHANNEL_ID/videos
 	CHANNEL_DIR="${VIDEO_PATH}/${CHANNEL_ID}"
-	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE $CHANNEL_URL -P $VIDEO_PATH -o '%(channel)s/%(title)s.%(ext)s'
+	yt-dlp -S "+res:720,ext" -ciw --download-archive $CACHE_PATH $CHANNEL_URL -P $VIDEO_PATH -o '%(channel)s/%(title)s.%(ext)s'
 fi
 
 ipfs add -r $CHANNEL_DIR
